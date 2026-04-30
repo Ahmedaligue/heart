5c8025c61b0db5f32414610df7bb5d1a9e507db2
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2026-04-30 23:41:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const heart = document.getElementById("heart");
+const wahyu = document.getElementById("wahyu");
+const rahmah = document.getElementById("rahmah");
+
+function animasi() {
+  wahyu.classList.toggle("animasi-wahyu");
+  rahmah.classList.toggle("animasi-rahmah");
+}
